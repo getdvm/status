eace01fd4bb5366aeadb829b3c6898e1d6f1005d
TimZhang <zewenzhang@gmail.com>
2016-07-21 14:47:50 +0800

    Init

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm777927708-55113ad0f1897355e3e5e703',
+	'm777927701-d64da2b9ab890fca2189c2fc',
+	'm777927692-bdedec8c957c6bd72f3d1659'
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
