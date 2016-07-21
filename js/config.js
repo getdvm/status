ebee1af2cd0790980d833c292615748945b48b71
TimZhang <zewenzhang@gmail.com>
2016-07-21 18:23:31 +0800

    Add monitor blog

--- js/config.js
+++ js/config.js
@@ -1,9 +1,10 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-  'm777927708-55113ad0f1897355e3e5e703',
-  'm777927701-d64da2b9ab890fca2189c2fc',
-  'm777927692-bdedec8c957c6bd72f3d1659',
-  'm777993918-3e78803b3187149db9b70bfc'
+  'm777927708-55113ad0f1897355e3e5e703', // Website
+  'm777927701-d64da2b9ab890fca2189c2fc', // Console
+  'm777927692-bdedec8c957c6bd72f3d1659', // Forum
+  'm777993918-3e78803b3187149db9b70bfc', // Api
+  'm777994075-3ff92a4f1c2c83db0d385fb0'  // Blog
 ];
 
 // refresh interval (in seconds)
