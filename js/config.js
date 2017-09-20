335f62638feb8698601d52e2bc780d9d7f9bb210
imeoer <imeoer@gmail.com>
2017-09-20 21:26:25 +0800

    add eu api status

--- js/config.js
+++ js/config.js
@@ -5,7 +5,8 @@ var __apiKeys = [
   'm777927692-bdedec8c957c6bd72f3d1659', // Forum
   'm777993918-3e78803b3187149db9b70bfc', // Console API
   'm777994075-3ff92a4f1c2c83db0d385fb0',  // Blog
-  'm779414840-738d202975b76ea8c7bcf335' // API (us-west-1)
+  'm779414840-738d202975b76ea8c7bcf335', // API (us-west-1)
+  'm779444614-2e39fff3c593eb0a4c35d3a8' // API (eu-central-1)
 ];
 
 // refresh interval (in seconds)
